ac9709947a1cd726501d41ed3736bcf72095fbca
Benjamin <Officialbenragland@gmail.com>
2024-02-11 03:01:51 -0500

    Adds countDown and writeCards to index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(x){
+    let thankYouArray=[]
+    for (let i=0 ; i< x.length ; i++){
+        thankYouArray.push(`Thank you, ${x[i]}, for the wonderful surprise gift!`)
+        debugger;
+    }
+   return thankYouArray
+}
+
+function countDown(x){
+    let count = 10
+    while(count >= 0){
+        console.log(count)
+        count--
+    }
+}
